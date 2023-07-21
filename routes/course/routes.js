42dfbdc26e00674a9c2056d2a51a53e49130cfd2
manasgupta01 <manasgupta7624@gmail.com>
2023-07-21 17:06:32 +0530

    comments

--- routes/course/routes.js
+++ routes/course/routes.js
@@ -1,12 +1,9 @@
 // External Import
 const express = require("express");
-
 const jwt = require("jsonwebtoken");
 // Database
 const {
   Course,
-  FEEDBACKTYPE_CODES,
-  REGISTRATIONSTATUS_CODES,
   MANAGERROLE_CODES,
 } = require("../../db/models/course/model");
 
@@ -14,7 +11,6 @@ const {
 const { createCourseValidator } = require("./validators");
 
 // Helpers
-// const checkUserRole = require('../../helpers/db').checkUserRole
 const { checkJwt } = require("../../helpers/jwt");
 const { generateResponseMessage } = require("../../helpers/response");
 
@@ -66,7 +62,13 @@ const router = express.Router();
  *       - bearerAuth: []
  *     responses:
  *       200:
- *         description: List of followers queries and returned
+ *         description: Successful operation. Returns the created course data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Course'
+ *       500:
+ *         description: Internal Server Error or Validation Error or Database Error.
  *         content:
  *           application/json:
  *             schema:
@@ -74,17 +76,23 @@ const router = express.Router();
  *               properties:
  *                 status:
  *                   type: string
- *                   description: success or error
- *                   example: success
- *                 data:
+ *                   example: error
+ *                 message:
  *                   type: string
- *                   description: confirmation message in case of "status" success
- *                   example: { status: "success" }
- *                   required: false
- *       500:
- *         description: Server error in contacting database
- *       204:
- *         description: Same as 200 but no data returned
+ *                   example: Internal Server Error or Validation Error or Database Error.
+ *       403:
+ *         description: Forbidden. The user does not have the required role for creating a course.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: error
+ *                 message:
+ *                   type: string
+ *                   example: Not allowed for this role.
  */
 router.post("/", async (req, res) => {
   const authHeader = req.headers["authorization"];
@@ -101,7 +109,6 @@ router.post("/", async (req, res) => {
   try {
     // Decode the JWT token to get the user information
     const decodedToken = jwt.verify(token, "aspireinfoz");
-    console.log("Decoded Token:", decodedToken);
     const { role, id } = decodedToken;
     const { title, subtitle, description, tags } = req.body;
 
@@ -133,11 +140,23 @@ router.post("/", async (req, res) => {
 
     res.status(200).json(generateResponseMessage("success", newCourseData));
   } catch (err) {
-    logger.error(err);
-    res.status(500).json(generateResponseMessage("error", err));
+    // Handle different types of errors and return appropriate response messages
+    if (err instanceof jwt.JsonWebTokenError) {
+      // JWT token error
+      return res.status(401).json(generateResponseMessage("error", "Invalid token"));
+    } else if (err instanceof jwt.TokenExpiredError) {
+      // JWT token expired error
+      return res.status(401).json(generateResponseMessage("error", "Token expired"));
+    } else if (err.name === "ValidationError") {
+      // Validation error in the request body
+      return res.status(500).json(generateResponseMessage("error", "Validation Error"));
+    } else {
+      // Other internal server errors
+      logger.error(err);
+      return res.status(500).json(generateResponseMessage("error", "Internal Server Error"));
+    }
   }
 });
-
 /**
  * @swagger
  * /course/all:
@@ -155,6 +174,26 @@ router.post("/", async (req, res) => {
  *               type: array
  *               items:
  *                 $ref: '#/components/schemas/Course'
+ *       204:
+ *         description: No Content. There are no courses found in the system.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: No courses found
+ *       400:
+ *         description: Bad Request. Invalid request parameters.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Invalid request parameters
  *       500:
  *         description: Internal Server Error. An error occurred while processing the request.
  *         content:
@@ -162,20 +201,32 @@ router.post("/", async (req, res) => {
  *             schema:
  *               type: object
  *               properties:
- *                 error:
+ *                 message:
  *                   type: string
- *                   example: Internal Server Error
+ *                   example: Internal Server Error.
  */
 router.get("/all", async (req, res) => {
   try {
     // Fetch all courses from the database
     const courses = await Course.find();
 
+    // Check if any courses were found
+    if (courses.length === 0) {
+      return res.status(204).json({ message: "No courses found" });
+    }
+
     // Return the courses as a JSON response
     res.status(200).json(courses);
   } catch (err) {
-    // If an error occurs, handle it gracefully
-    res.status(500).json({ error: "Internal Server Error" });
+    // Handle different types of errors and return appropriate response messages
+    if (err.name === "MongoError" && err.code === 18) {
+      // Mongoose Validation Error
+      return res.status(400).json({ message: "Invalid request parameters" });
+    } else {
+      // Other internal server errors
+      console.error("Error fetching courses:", err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
   }
 });
 
@@ -216,6 +267,16 @@ router.get("/all", async (req, res) => {
  *                 message:
  *                   type: string
  *                   example: Unauthorized, missing auth token.
+ *       404:
+ *         description: Not Found. The course with the specified ID was not found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Course not found
  *       500:
  *         description: Internal Server Error. An error occurred while processing the request.
  *         content:
@@ -231,39 +292,47 @@ router.get("/all", async (req, res) => {
  *                   example: Internal Server Error.
  */
 router.get("/details/:id", async (req, res) => {
-  // console.log("Route reached");
   const authHeader = req.headers["authorization"];
-  //console.log(authHeader);
   const token = authHeader && authHeader.split(" ")[1];
-  // console.log(token);
+
   if (!token) {
-    return res
-      .status(401)
-      .json(
-        generateResponseMessage("error", "Unauthorized, missing auth token.")
-      );
+    return res.status(401).json(generateResponseMessage("error", "Unauthorized, missing auth token."));
   }
 
-  // const userId = req.userId // from the middleware checkJWT
-  // const userExists = await isUserActive(userId)
-  // // only permanent users can access the courses
-  // if (userExists !== 1){
-  //     const outputString =  "The user's account is " +
-  //         (userExists == -1) ? "throwing a db error" :
-  //         (userExists == -2) ? "not a valid id" :
-  //         (userExists == 0) ? "not found with this id" :
-  //         (userExists == 2) ? "is only temporarily registered" :
-  //         (userExists == 3) ? "is banned" : `!! returning ${userExists}`
-  //     res.status(403).json(generateResponseMessage("error", outputString))
-  // }
   try {
-    const course = await Course.findById(req.params.id);
-    if (!course) throw Error("Course not found");
+    // Decode the JWT token to get the user information
+    const decodedToken = jwt.verify(token, "aspireinfoz");
+    console.log("Decoded Token:", decodedToken);
+
+    // Check if user exists and is active (example code commented out)
+    // const userId = decodedToken.id;
+    // const userExists = await isUserActive(userId);
+    // // only permanent users can access the courses
+    // if (userExists !== 1) {
+    //     const outputString =  "The user's account is " +
+    //         (userExists == -1) ? "throwing a db error" :
+    //         (userExists == -2) ? "not a valid id" :
+    //         (userExists == 0) ? "not found with this id" :
+    //         (userExists == 2) ? "is only temporarily registered" :
+    //         (userExists == 3) ? "is banned" : `!! returning ${userExists}`
+    //     return res.status(403).json(generateResponseMessage("error", outputString));
+    // }
+
+    const courseId = req.params.id;
+    const course = await Course.findById(courseId);
+
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    // Return the course details
     res.status(200).json(course);
   } catch (err) {
-    res.status(500).json({ msg: err.message });
+    console.error("Error fetching course details:", err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
 
 
 
